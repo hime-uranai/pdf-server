524ac8cde0f41189cfce6149fdd55ee98fc92ce3
hime-uranai <takero.360@gmail.com>
2026-04-19 14:23:25 +0900

    fix puppeteer final

--- server.js
+++ server.js
@@ -13,14 +13,12 @@ app.get('/pdf', async (req, res) => {
   try {
     const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      headless: true,
     });
 
     const page = await browser.newPage();
 
     await page.goto(url, {
-      waitUntil: 'domcontentloaded',
-      timeout: 30000,
+      waitUntil: 'networkidle0',
     });
 
     const pdf = await page.pdf({
