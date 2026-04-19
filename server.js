b25d750259c63e90ad701233aec640f02865c8f7
hime-uranai <takero.360@gmail.com>
2026-04-19 12:36:03 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const app = express();
+
+app.get('/pdf', async (req, res) => {
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).send('URL is required');
+  }
+
+  try {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto(url, {
+      waitUntil: 'networkidle0',
+    });
+
+    const pdf = await page.pdf({
+      format: 'A4',
+      printBackground: true,
+    });
+
+    await browser.close();
+
+    res.set({
+      'Content-Type': 'application/pdf',
+    });
+
+    res.send(pdf);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('PDF generation failed');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
