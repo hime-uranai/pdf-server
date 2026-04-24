0294b2d453fb70985df948665eaf5f6f1906818d
hime-uranai <takero.360@gmail.com>
2026-04-24 18:12:10 +0900

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.post("/pdf", async (req, res) => {
    
 const pdf = await page.pdf({
   width: "430px",
-  height: "650px",
+  height: "700px",
   printBackground: true,
   scale: 2,
   margin: {
